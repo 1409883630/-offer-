b6359236e8a547ab21e369e17f8bec66df0f5845
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-08-05 23:07:13 +0800

    二进制中1的个数

---
+++ offer 15.java
@@ -0,0 +1,15 @@
+//剑指 Offer 15. 二进制中1的个数
+//编写一个函数，输入是一个无符号整数（以二进制串的形式），
+//返回其二进制表达式中数字位数为 '1' 的个数（也被称为 汉明重量).）。
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+         int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+}
