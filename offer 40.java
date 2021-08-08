7652a9134ac646f37b79ced8d90466f779cfd0a2
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-08-08 23:32:49 +0800

    最小的k个数

---
+++ offer 40.java
@@ -0,0 +1,14 @@
+//剑指offer 40 最小的k个数
+//输入整数数组 arr ，找出其中最小的 k 个数。
+//例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+class Solution {
+    //对原数组从小到大排序后取出前 kk 个数即可
+    public int[] getLeastNumbers(int[] arr, int k) {
+        int[] vec = new int[k];
+        Arrays.sort(arr);
+        for (int i = 0; i < k; ++i) {
+            vec[i] = arr[i];
+        }
+        return vec;
+    }
+}
