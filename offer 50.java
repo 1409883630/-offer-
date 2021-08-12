ee6237948e0e893007e627a72ded37451810592a
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-08-12 23:34:04 +0800

    第一个只出现一次的字符

--- offer 50.java
+++ offer 50.java
@@ -1,11 +1,11 @@
-//��ָ Offer 50. ��һ��ֻ����һ�ε��ַ�
-//���ַ��� s ���ҳ���һ��ֻ����һ�ε��ַ������û�У�����һ�����ո� s ֻ����Сд��ĸ��
+//剑指 Offer 50. 第一个只出现一次的字符
+//在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个空格。 s 只包含小写字母。
 class Solution {
     public char firstUniqChar(String s) {
        for (int i = 0; i < s.length(); i++) {
            char ch=s.charAt(i);
            
-           //�״γ��ֵ�λ���ǵ�ǰλ�ã��Һ���û���ٳ�������ַ�
+           //首次出现的位置是当前位置，且后面没有再出现这个字符
             if(s.indexOf(ch)==i&&s.indexOf(ch,i+1)==-1)
                 return s.charAt(i);
         }
