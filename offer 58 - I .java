9b0e6924df6a0f600e92d7a3b1bcde725f3c7a0a
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-10-18 23:16:41 +0800

    剑指 Offer 58 - I. 翻转单词顺序

---
+++ offer 58 - I .java
@@ -0,0 +1,26 @@
+//剑指 Offer 58 - I. 翻转单词顺序
+//输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+//为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+class Solution {
+    public String reverseWords(String s) {
+      //将传进来的字符串以空格拆分
+        String[] strings = s.trim().split(" ");
+        StringBuffer stringBuffer = new StringBuffer();
+        //从尾巴开始遍历
+        for (int i = strings.length - 1; i >= 0; i--) {
+            if (strings[i].equals("")) {
+                continue;
+            }
+            //到头了，append然后去空格
+            if (i == 0) {
+                stringBuffer.append(strings[i].trim());
+            } else {
+                // 怕有多余的空格，去掉，再加上去
+                stringBuffer.append(strings[i].trim()).append(" ");
+            }
+        }
+        //输出String 完事，安排！
+        return stringBuffer.toString();
+
+    }
+}
